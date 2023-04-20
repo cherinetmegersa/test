347b1c6e6d904284ee6e42d20d1afd45df8ca3c9
cherinetmegersa <128375181+cherinetmegersa@users.noreply.github.com>
2023-04-20 04:21:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function finishfunction(){
+    alert ("Thank you for complating the quiz ")
+}
+function checkAnswer(){
+    var score = 0;
+    //assign the quiz elemnts to the quiz variable
+    quiz = docement.forms.quiz.elements;
+    //assgn the answer to the quastion to the aswer variable
+    answer = quiz.growth.value;
+    //check the answer to the first question
+    if (answer=="70%"){
+        //add 1 to the score
+        score = score +1;
+    }
+    answer2 = quiz.talent.value;
+    if (answer2=="50")(
+        score = score +1
+    )
+    alert ("well done your answer was amazing")
+    score;
+
+}
---
+++ story.js
@@ -0,0 +1,17 @@
+console.log("Hello, world");
+
+//VARIABLES TO CHANGE
+var myname = ("cherinet");
+var myage = (18);
+var favcolour = ("yellow")
+console.log("hello" + myname);
+
+// STORY - DO NOT CHANGE
+if(myage<33)
+{console.log("you're" +myage +"?wow!")
+}
+if(favcolour == "yellow"){
+    console.log("my favourite colour is yellow too")
+}
+
+
